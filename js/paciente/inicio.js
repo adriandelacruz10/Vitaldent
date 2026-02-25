3580cc4d762990d776dd965f1ce413e236040b38
analistapr02 <analistapr02@plasticaucho.com>
2026-02-25 10:27:14 -0500

    Agregar funcion para eliminar pacientes sin facturas

--- js/paciente/inicio.js
+++ js/paciente/inicio.js
@@ -42,6 +42,10 @@ $(document).ready(function () {
     $("#btnMorosos").click(function () {
         window.open("morosos.html");
     })
+    //Boton para mostrar todos los pacientes
+    $("#btnTodos").click(function () {
+        cargarPacientes();
+    })
 
     //FOCUS
     //Dar focus al input nombre
@@ -59,14 +63,12 @@ $(document).ready(function () {
         var mensaje = "<td colspan='7' align='center'>Bienvenidos al sistema odontológico para la gestión de pacientes." +
             "<br><b>VITALDENT TU SONRISA, TU IMAGEN</b></td>";
         $("#listado").html(mensaje);
-        //var i = 1;
-        var pacientesData = [];
-        var referencia = database.ref("Pacientes");
+        var referencia = database.ref("Pacientes").orderByChild('nom_pac');
         referencia.on("value", function (datos) {
-            pacientesData = datos.val();
-            $.each(pacientesData, function (indice, valor) {
+            datos.forEach(function(snap){
                 var paciente = new Object();
-                paciente.id = indice;
+                var valor = snap.val();
+                paciente.id = snap.key;
                 paciente.fecha = valor.fec_pac;
                 paciente.cedula = valor.ced_pac;
                 paciente.nombre = valor.nom_pac;
@@ -74,122 +76,71 @@ $(document).ready(function () {
                 paciente.telefono = valor.tel1_pac;
                 paciente.observacion = valor.obs_pac;
                 paciente.plan = valor.pla_pac;
-                //paciente.num = i;
                 pacientes.push(paciente);
-                //i++;
-            })
+            });
         });
     }
+    
     //Cargar todos los pacientes
     function cargarPacientes() {
         $("#listado").empty();
         var i = 1;
-        var pacientesData = [];
-        var referencia = database.ref("Pacientes");
-        referencia.on("value", function (datos) {
-            pacientesData = datos.val();
-            $.each(pacientesData, function (indice, valor) {
-                var cadena = "<tr>";
-                cadena += "<th>" + i + "</th>";
-                cadena += "<td>" + valor.nom_pac + "</td>";
-                var edad = calcularEdad(valor.fec_pac);
-                cadena += "<td>" + edad + "</td>";
-                cadena += "<td>" + valor.tel1_pac + "</td>";
-                cadena += "<td>" + valor.pla_pac + "</td>";
-                cadena += "<td>" + valor.obs_pac + "</td>";
-                cadena += "<td> <button type='button' class='btnVer btn btn-outline-primary btn-sm'" + "id='" + indice + "'>" +
-                    "Ver </button> ";
-                cadena += "<button type='button' class='btnCitas btn btn-outline-dark btn-sm'" + "id='" + valor.ced_pac + "*" + valor.nom_pac + "*" + valor.tel1_pac + "'>" +
-                    "Citas </button> ";
-                cadena += "<button type='button' class='btnCaja btn btn-outline-success btn-sm'" + "id='" + valor.ced_pac + "*" + valor.nom_pac + "'>" +
-                    "Caja </button></td>";
-                cadena += "</tr>"
-                $(cadena).appendTo("#listado");
-                var paciente = new Object();
-                paciente.id = indice;
-                paciente.fecha = valor.fec_pac;
-                paciente.cedula = valor.ced_pac;
-                paciente.nombre = valor.nom_pac;
-                paciente.direccion = valor.dir_pac;
-                paciente.telefono = valor.tel1_pac;
-                paciente.observacion = valor.obs_pac;
-                paciente.plan = valor.pla_pac;
-                paciente.num = i;
-                pacientes.push(paciente);
-                i++;
-            })
 
-            //Botones
-            $(".btnVer").click(function () {
-                var id = this.id;
-                localStorage.setItem("idPaciente", id);
-                location.assign("informacion.html");
-            })
-            $(".btnCitas").click(function () {
-                var datos = this.id.split("*");
-                localStorage.setItem("cedulaPaciente", datos[0]);
-                localStorage.setItem("nombrePaciente", datos[1]);
-                localStorage.setItem("telefonoPaciente", datos[2]);
-                location.assign("citas.html");
-            })
-            $(".btnCaja").click(function () {
-                var datos = this.id.split("*");
-                localStorage.setItem("cedulaPaciente", datos[0]);
-                localStorage.setItem("nombrePaciente", datos[1]);
-                location.assign("caja.html");
-            })
-        });
-    }
-    //Buscar paciente por numero de cedula
-    function buscarPaciente() {
-        var cedula = $("#txtNumeroCedula").val();
-        var paciente = pacientes.find(paciente => paciente.cedula === cedula);
-        if (paciente == null) {
-            alert("El paciente no existe.");
-            $("#txtNumeroCedula").val("");
-            $("#txtNombre").val("");
-        } else {
-            $("#listado").empty();
+        pacientes.forEach(valor => {
             var cadena = "<tr>";
-            cadena += "<th>" + paciente.num + "</th>";
-            cadena += "<td>" + paciente.nombre + "</td>";
-            var edad = calcularEdad(paciente.fecha);
+            cadena += "<th>" + i + "</th>";
+            cadena += "<td>" + valor.nombre + "</td>";
+            var edad = calcularEdad(valor.fecha);
             cadena += "<td>" + edad + "</td>";
-            cadena += "<td>" + paciente.telefono + "</td>";
-            cadena += "<td>" + paciente.plan + "</td>";
-            cadena += "<td>" + paciente.observacion + "</td>";
-            cadena += "<td> <button type='button' class='btnVer btn btn-outline-primary btn-sm'" + "id='" + paciente.id + "'>" +
+            cadena += "<td>" + valor.telefono + "</td>";
+            cadena += "<td>" + valor.plan + "</td>";
+            cadena += "<td>" + valor.observacion + "</td>";
+            cadena += "<td> <button type='button' class='btnVer btn btn-outline-primary btn-sm'" + "id='" + valor.id + "'>" +
                 "Ver </button> ";
-            cadena += "<button type='button' class='btnCitas btn btn-outline-dark btn-sm'" + "id='" + paciente.cedula + "*" + paciente.nombre + "*" + paciente.telefono + "'>" +
+            cadena += "<button type='button' class='btnCitas btn btn-outline-dark btn-sm'" + "id='" + valor.cedula + "*" + valor.nombre + "*" + valor.telefono + "'>" +
                 "Citas </button> ";
-            cadena += "<button type='button' class='btnCaja btn btn-outline-success btn-sm'" + "id='" + paciente.cedula + "*" + paciente.nombre + "'>" +
-                "Caja </button></td>";
+            cadena += "<button type='button' class='btnCaja btn btn-outline-success btn-sm'" + "id='" + valor.cedula + "*" + valor.nombre + "'>" +
+                "Caja </button> ";
+            cadena += "<button type='button' class='btnEliminar btn btn-outline-danger btn-sm'" + "id='" + valor.id + "'>" +
+                "Eliminar </button></td>";
             cadena += "</tr>"
             $(cadena).appendTo("#listado");
+            i++;
+        });
 
-            //Botones
-            $(".btnVer").click(function () {
-                var id = this.id;
-                localStorage.setItem("idPaciente", id);
-                location.assign("informacion.html");
-            })
-            $(".btnCitas").click(function () {
-                var datos = this.id.split("*");
-                localStorage.setItem("cedulaPaciente", datos[0]);
-                localStorage.setItem("nombrePaciente", datos[1]);
-                localStorage.setItem("telefonoPaciente", datos[2]);
-                location.assign("citas.html");
-            })
-            $(".btnCaja").click(function () {
-                var datos = this.id.split("*");
-                localStorage.setItem("cedulaPaciente", datos[0]);
-                localStorage.setItem("nombrePaciente", datos[1]);
-                location.assign("caja.html");
-            })
-        }
+        //Botones
+        $(".btnVer").click(function () {
+            var id = this.id;
+            localStorage.setItem("idPaciente", id);
+            location.assign("informacion.html");
+        })
+        $(".btnCitas").click(function () {
+            var datos = this.id.split("*");
+            localStorage.setItem("cedulaPaciente", datos[0]);
+            localStorage.setItem("nombrePaciente", datos[1]);
+            localStorage.setItem("telefonoPaciente", datos[2]);
+            location.assign("citas.html");
+        })
+        $(".btnCaja").click(function () {
+            var datos = this.id.split("*");
+            localStorage.setItem("cedulaPaciente", datos[0]);
+            localStorage.setItem("nombrePaciente", datos[1]);
+            location.assign("caja.html");
+        })
+        $(".btnEliminar").click(function () {
+            var id = this.id;
+            if(confirm('¿Esta seguro de eliminar al paciente seleccionado') == true){
+                eliminarPaciente(id).then(() =>{
+                    pacientes = pacientes.filter(p => p.id !== id);
+                    cargarPacientes();
+                });
+            }
+        })
     }
 
+    //Buscar paciente por tratamiento
     function buscarPacienteTratamiento() {
+        $("#listado").empty();
         var tra = $("#txtBuscarTratamiento").val();
         var tras = tra.split(" ");
         var aux = false;
@@ -225,7 +176,11 @@ $(document).ready(function () {
                 cadena += "<button type='button' class='btnCitas btn btn-outline-dark btn-sm'" + "id='" + paciente.cedula + "*" + paciente.nombre + "*" + paciente.telefono + "'>" +
                     "Citas </button> ";
                 cadena += "<button type='button' class='btnCaja btn btn-outline-success btn-sm'" + "id='" + paciente.cedula + "*" + paciente.nombre + "'>" +
-                    "Caja </button></td>";
+                    "Caja </button> ";
+                /*cadena += "<button type='button' class='btnOdontograma btn btn-outline-info btn-sm'" + "id='" + paciente.id + "*" + paciente.nombre + "'>" +
+                    "Odo. </button> ";*/
+                cadena += "<button type='button' class='btnEliminar btn btn-outline-danger btn-sm'" + "id='" + paciente.id + "'>" +
+                    "Elim. </button></td>";
                 cadena += "</tr>"
                 aux = true;
                 i++;
@@ -252,7 +207,22 @@ $(document).ready(function () {
                 localStorage.setItem("cedulaPaciente", datos[0]);
                 localStorage.setItem("nombrePaciente", datos[1]);
                 location.assign("caja.html");
-            })
+            });
+            $(".btnOdontograma").click(function () {
+                var datos = this.id.split("*");
+                localStorage.setItem("idPaciente", datos[0]);
+                localStorage.setItem("nombrePaciente", datos[1]);
+                location.assign("odontograma.html");
+            });
+            $(".btnEliminar").click(function () {
+                var id = this.id;
+                if(confirm('¿Esta seguro de eliminar al paciente seleccionado') == true){
+                    eliminarPaciente(id).then(() => {
+                        pacientes = pacientes.filter(p => p.id !== id);
+                        buscarPacienteTratamiento();
+                    });
+                }
+            });
         } else {
             var alerta = "<div class='alert alert-danger'>";
             alerta += "<a class='close' data-dismiss='alert'> × </a> <strong> Tratamiento no encontrado! Revise la información e inténtelo de nuevo.</strong>";
@@ -265,6 +235,7 @@ $(document).ready(function () {
 
     //Buscar paciente por nombre
     function buscarPacienteNombre() {
+        $("#listado").empty();
         var nombre = $("#txtNombre").val();
         var nombres = nombre.split(" ");
         var aux = false;
@@ -301,9 +272,11 @@ $(document).ready(function () {
                     "Citas </button> ";
                 cadena += "<button type='button' class='btnCaja btn btn-outline-success btn-sm'" + "id='" + paciente.cedula + "*" + paciente.nombre + "'>" +
                     "Caja </button> ";
-                cadena += "<button type='button' class='btnOdontograma btn btn-outline-info btn-sm'" + "id='" + paciente.id + "*" + paciente.nombre + "'>" +
-                    "Odo. </button></td>";
-                cadena += "</tr>"
+                /*cadena += "<button type='button' class='btnOdontograma btn btn-outline-info btn-sm'" + "id='" + paciente.id + "*" + paciente.nombre + "'>" +
+                    "Odo. </button> ";*/
+                cadena += "<button type='button' class='btnEliminar btn btn-outline-danger btn-sm'" + "id='" + paciente.id + "'>" +
+                    "Elim. </button></td>";
+                cadena += "</tr>";
                 aux = true;
                 i++;
             }
@@ -335,7 +308,16 @@ $(document).ready(function () {
                 localStorage.setItem("idPaciente", datos[0]);
                 localStorage.setItem("nombrePaciente", datos[1]);
                 location.assign("odontograma.html");
-            })
+            });
+            $(".btnEliminar").click(function () {
+                var id = this.id;
+                if(confirm('¿Esta seguro de eliminar al paciente seleccionado') == true){
+                    eliminarPaciente(id).then(() => {
+                        pacientes = pacientes.filter(p => p.id !== id);
+                        buscarPacienteNombre();
+                    });
+                }
+            });
         } else {
             var alerta = "<div class='alert alert-danger'>";
             alerta += "<a class='close' data-dismiss='alert'> × </a> <strong> Paciente no encontrado! Revise la información e inténtelo de nuevo. </strong>";
@@ -378,6 +360,11 @@ $(document).ready(function () {
         return fechaActual;
     }
 
+    //Eliminar el paciente por el id
+    function eliminarPaciente(id){
+        var referencia = database.ref('Pacientes');
+        referencia.child(id).remove();
+    }
     //Carga el nombre y la imagen de usuario
     function cargarUsuario() {
         var nombre = "<p>Elizabeth Bastidas</p>";
@@ -389,17 +376,3 @@ $(document).ready(function () {
         })
     }
 })
-
-/*class Paciente {
-    constructor(nombre, cedula, genero, fecha, tel1, tel2, correo, direccion, obs) {
-        this.nombre = nombre;
-        this.cedula = cedula;
-        this.genero = genero;
-        this.fecha = fecha;
-        this.tel1 = tel1;
-        this.tel2 = tel2;
-        this.correo = correo;
-        this.direccion = direccion;
-        this.obs = obs;
-    }
-}*/
